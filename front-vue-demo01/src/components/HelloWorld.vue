ff543227307e48829d647880737e8cfbb91b357c
yuyifeifei <luoletter2000@163.com>
2018-09-18 20:31:51 +0800

    vue-demo 跨域
    简单跨域处理，开发阶段

---
+++ front-vue-demo01/src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'demo'
+    }
+  },
+  mounted () {
+    var that = this
+    that.$http.get('/article/get/3')
+    .then(function (result) {
+      that.msg = result.body.desc
+      console.log(result.body)
+    }, function (error) {
+      console.log('请求失败' + error)
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
